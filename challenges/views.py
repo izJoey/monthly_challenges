07928e7768fb0c23efc489505acfa2235e75568a
Joey <joeyrjtd@gmail.com>
2023-02-24 15:47:18 -0300

    first commit

---
+++ challenges/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+# Create your views here.
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes a day!",
+    "march": "Learn django for at least 20 minutes a day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes a day!",
+    "june": "Learn django for at least 20 minutes a day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes a day!",
+    "september": "Learn django for at least 20 minutes a day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes a day!",
+    "december": "Learn django for at least 20 minutes a day!"
+}
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
